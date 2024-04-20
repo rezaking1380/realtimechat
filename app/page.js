76bbfc315a0c49f3c2922be55cda4267d0ad622b
reza <83880517+rezaking1380@users.noreply.github.com>
2024-04-20 08:58:19 -0700

    fix all project

--- app/page.js
+++ app/page.js
@@ -1,7 +1,51 @@
-import Image from "next/image";
+"use client"
+
+import { app, firestore } from "@/lib/firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import Users from "./components/User";
+import ChatRoom from "./components/ChatRoom";
 
 export default function Home() {
+  const auth = getAuth(app)
+  const [user, setUser] = useState(null)
+  const [selectedChatroom, setSelectedChatroom] = useState(null);
+  const router = useRouter()
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if(user) {
+        const userRef = doc(firestore, 'users', user.uid)
+        const userSnap = await getDoc(userRef)
+        const userData = ({id: userSnap.id, ...userSnap.data()})
+        setUser(userData)
+      } else {
+        setUser(null)
+        router.push('/login')
+      }
+    })
+    return () => unsubscribe()
+  }, [auth,router])
+  if(user == null) return (<div className='text-4xl'>Loading...</div>);
   return (
-    <div></div>
+    <div className="flex h-screen">
+      <div className="flex-shrink-0 sm:w-3/12 w-1/12">
+        <Users userData={user} setSelectedChatroom={setSelectedChatroom}/>
+      </div>
+
+      <div className="flex-grow w-9/12">
+        {
+          selectedChatroom ? (<>
+          <ChatRoom user={user} selectedChatroom={selectedChatroom}/>
+          </>):(<>
+          <div className="flex items-center justify-center h-full">
+            <div className="text-2xl text-gray-400">Select a chatroom</div>
+          </div>
+          </>)
+        }
+        
+      </div>
+    </div>
   );
 }
